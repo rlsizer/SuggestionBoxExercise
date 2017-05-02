80e2079589da7987b8b6fe354f3542da92e2da55
rlsizer <theryanlilo@gmail.com>
2017-05-02 11:46:00 -0400

    Scaffolded suggestion controller.

---
+++ SuggestionBox/Controllers/SuggestionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SuggestionBox.Models;
+
+namespace SuggestionBox.Controllers
+{
+    public class SuggestionsController : Controller
+    {
+        private SuggestionBoxContext db = new SuggestionBoxContext();
+
+        // GET: Suggestions
+        public ActionResult Index()
+        {
+            return View(db.Suggestions.ToList());
+        }
+
+        // GET: Suggestions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Suggestion suggestion = db.Suggestions.Find(id);
+            if (suggestion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(suggestion);
+        }
+
+        // GET: Suggestions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Suggestions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Comment")] Suggestion suggestion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Suggestions.Add(suggestion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(suggestion);
+        }
+
+        // GET: Suggestions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Suggestion suggestion = db.Suggestions.Find(id);
+            if (suggestion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(suggestion);
+        }
+
+        // POST: Suggestions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Comment")] Suggestion suggestion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(suggestion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(suggestion);
+        }
+
+        // GET: Suggestions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Suggestion suggestion = db.Suggestions.Find(id);
+            if (suggestion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(suggestion);
+        }
+
+        // POST: Suggestions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Suggestion suggestion = db.Suggestions.Find(id);
+            db.Suggestions.Remove(suggestion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SuggestionBox/Models/Suggestion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuggestionBox.Models
+{
+    public class Suggestion
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
